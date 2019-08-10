f18bef8ff9c4c237cf0ed1a02d29afcbfbb44e3f
Stephen-Baxter <52463998+Stephen-Baxter@users.noreply.github.com>
2019-08-10 15:03:23 -0500

    Create Headers_and_appINFO.h

---
+++ CLI/RPG Dice/Headers_and_appINFO.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream> //for std::cout and std::endl
+#include <vector> //for std::vector
+#include <random>//for random numbers
+
+#include <conio.h> //for _getch()
+#include <Windows.h> //for Sleep() and reseting the console
+
+//******************************************************************************************************
+//Author: Stephen Baxter
+//Development Tool Used: Visual Studeo 2019
+//Operating System Used: Windows 8.1
+//Name: RPG Dice
+//Language: c++
+//
+//Description: RPG Dice is a comand line interface program to be use in place of a physical
+//four, six, eight, ten, twelve, or twenty sided Die.
+//******************************************************************************************************
